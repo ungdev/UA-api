15381bd7444e27d4e48a860b2bc83fc887a4e645
Nathan <nathan.thibault@utt.fr>
2020-09-17 20:38:07 +0200

    fix: change request to BodyRequest<User>

--- src/middlewares/user.ts
+++ src/middlewares/user.ts
@@ -1,9 +1,10 @@
 import { IRoute, NextFunction, Request, Response } from 'express';
 import log from '../utils/log';
+import { BodyRequest, User } from '../types';
 
 // Check the user's team. If he's in one, it will return an error.
-export const isNotInATeam = () => async (req: Request, res: Response, next: NextFunction) => {
-  if (req.user.team) {
+export const isNotInATeam = () => async (req: BodyRequest<User>, res: Response, next: NextFunction) => {
+  if (req.body.teamId) {
     log.debug(`${req.path} failed : already in team`);
 
     return res
