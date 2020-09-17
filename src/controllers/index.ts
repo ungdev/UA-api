76daaae2f81d97687a826f92f951c82006105492
ValentinPeltier <valentinpeltier@yahoo.fr>
2020-09-17 20:35:57 +0200

    feat: start implementing /users routes

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import status from './status';
+import users from './users';
 
 const routes = (): Router => {
   const router = Router();
 
   // To match only with root
-  router.use('//', status());
+  router.get('//', status());
+
+  // Users routes
+  router.use('/users', users());
 
   return router;
 };
