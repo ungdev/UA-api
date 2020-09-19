dbc3e7dc68933a1b3bfe98d4e46d0c19858493cc
Thomas de Lachaux <thomas.delachaux@gmail.com>
2020-09-19 17:20:20 +0200

    Correct database connection

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import { databaseHost, databaseName, databasePassword, databasePort, databaseUsername } from './environment';
 import log from './log';
 
 const database = new PrismaClient({
@@ -7,6 +8,7 @@ const database = new PrismaClient({
 
 // Dump query to initiate the connection
 const setup = async () => {
+  process.env.DATABASE_URL = `mysql://${databaseUsername()}:${databasePassword()}@${databaseHost()}:${databasePort()}/${databaseName()}`;
   await database.settings.findMany();
 };
 
