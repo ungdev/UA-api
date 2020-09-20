6ffa90fb7446572b04d3899baefd453c7e905137
Thomas de Lachaux <thomas.delachaux@gmail.com>
2020-09-20 20:30:38 +0200

    Correct log level

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,3 @@
-/* eslint-disable import/first */
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+/* eslint-disable import/first */
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import { apiPort, nodeEnv as nodeEnvironment } from './utils/environment';
 import log from './utils/log';
 import app from './app';
 
 // Listen the API on port 3000 (default)
 app.listen(apiPort(), () => {
-  log.info(`Node environment: ${nodeEnvironment()}`);
-  log.info(`Listening on ${apiPort()}...`);
+  log.debug(`Node environment: ${nodeEnvironment()}`);
+  log.debug(`Listening on ${apiPort()}...`);
 });
