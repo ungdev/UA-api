99436214ef17bc32651f3e423dda0e113c15b15e
florianess <florian.esslinger@live.fr>
2020-09-20 19:20:39 +0200

    feat: start auth test

--- tests/general.test.ts
+++ tests/general.test.ts
@@ -1,9 +1,6 @@
-import chai from 'chai';
 import request from 'supertest';
 import app from '../src/app';
 import database from '../src/utils/database';
-
-chai.should();
 
 describe('General API', () => {
   describe('GET /', () => {
