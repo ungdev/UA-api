367fe496c53457f0932d551ba242e40ea44a5ce0
florianess <florian.esslinger@live.fr>
2020-09-20 12:01:17 +0200

    feat: first test

---
+++ test/general.test.ts
@@ -0,0 +1,25 @@
+import chai from 'chai';
+import request from 'supertest';
+import app from '../src/app';
+import database from '../src/utils/database';
+
+chai.should();
+
+describe('General API', () => {
+  describe('GET /', () => {
+    it('should return 200 with an object', async () => {
+      await request(app).get('/').expect(200, { shop: false, login: false });
+    });
+    it('should return the updated value', async () => {
+      await database.settings.update({
+        where: {
+          id: 'login',
+        },
+        data: {
+          value: 'true',
+        },
+      });
+      await request(app).get('/').expect(200, { shop: false, login: true });
+    });
+  });
+});
