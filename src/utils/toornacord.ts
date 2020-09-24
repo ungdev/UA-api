46c9c3a667528d895da8c0853fa3ba81bf1d85ef
Thomas de Lachaux <thomas.delachaux@gmail.com>
2020-09-25 00:20:16 +0200

    WIP : Add Discord create channel/roles function

--- src/utils/toornacord.ts
+++ src/utils/toornacord.ts
@@ -1,8 +1,11 @@
+import Discord from 'discord.js';
 import { Request } from 'express';
 import axios from 'axios';
 import { toornamentCredentials } from './toornament';
 import { PlayerToornament, WebhookToornament } from '../types';
 import logger from './log';
+import { fetchTournament } from '../operations/tournament';
+import { discordServer, discordToken } from './environment';
 
 const baseURL = 'https://api.toornament.com/organizer/v2/tournaments';
 
@@ -40,4 +43,68 @@ export default async (request: Request) => {
     default:
       break;
   }
+};
+
+const bot = new Discord.Client();
+let server: Discord.Guild;
+
+if (discordToken()) {
+  bot.login(discordToken());
+  bot.on('ready', () => logger.info('Bot ready'));
+
+  server = bot.guilds.cache.get(discordServer());
+} else {
+  logger.warn('Discord token not entered, you can still continue to dev !');
+}
+
+const getMember = (id: string) => {
+  return server.members.cache.find((member) => id === `${member.user.username}#${member.user.discriminator}`);
+};
+
+/**
+ * Create a discord team with 2 channels and assign roles
+ */
+export const createTeam = async (team: string, discordIds: Array<string>, tournamentId: string) => {
+  const tournament = await fetchTournament(tournamentId);
+
+  const discordName = `${tournamentId}_${team}`;
+
+  const tournamentRole = server.roles.cache.get(tournament.discordRoleId);
+
+  // Create role
+  const teamRole = await server.roles.create({
+    data: {
+      name: discordName,
+    },
+  });
+
+  // Assign role to all the users
+  await Promise.all(
+    discordIds.map((discordId) => {
+      const member = getMember(discordId);
+      return member.roles.add([teamRole, tournamentRole]);
+    }),
+  );
+
+  // Create channels
+  const channelTypes: Array<'text' | 'voice'> = ['text', 'voice'];
+
+  await Promise.all(
+    channelTypes.map((channelType) =>
+      server.channels.create(discordName, {
+        type: channelType,
+        parent: tournament.discordCategoryId,
+        permissionOverwrites: [
+          {
+            id: server.roles.everyone.id,
+            deny: ['VIEW_CHANNEL'],
+          },
+          {
+            id: teamRole.id,
+            allow: ['VIEW_CHANNEL'],
+          },
+        ],
+      }),
+    ),
+  );
 };
