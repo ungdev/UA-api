02c3e84a895970830e0feec065ee7820a8d0c718
Lucien <Saperli000@gmail.com>
2020-09-28 22:51:14 +0200

    Update parameters.ts
    isUserId first release

--- src/middlewares/parameters.ts
+++ src/middlewares/parameters.ts
@@ -1,16 +1,26 @@
-import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
-import { badRequest } from '../utils/responses';
-import log from '../utils/log';
+import { Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { unauthenticated, unauthorized } from '../utils/responses';
+import { getToken } from '../utils/user';
+import { jwtSecret } from '../utils/environment';
+import { Token, UserRequest } from '../types';
 
-export const isCaptainOfTeamId = (request: Request, response: Response, next: NextFunction): void => {
-  return next();
+// export const isCaptainOfTeamId = (request: Request, response: Response, next: NextFunction): void => {
+//   return next();
+// };
+
+export const isUserId = (request: UserRequest, response: Response, next: NextFunction): void => {
+  const token = getToken(request);
+  if (token) {
+    const decoded = jwt.verify(token, jwtSecret()) as Token;
+    if (decoded.userId.toString() === request.user.id) {
+      return next();
+    }
+    return unauthorized(response);
+  }
+  return unauthenticated(response);
 };
 
-export const isUserId = (request: Request, response: Response, next: NextFunction): void => {
-  log.debug('tyyy');
-  return next();
-};
-
-export const isInTeamId = (request: Request, response: Response, next: NextFunction): void => {
-  return next();
-};
+// export const isInTeamId = (request: Request, response: Response, next: NextFunction): void => {
+//   return next();
+// };
