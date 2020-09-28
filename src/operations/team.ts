1471a6af87aaa1329224eb5971de604b77d022eb
Lucien <Saperli000@gmail.com>
2020-09-28 23:33:40 +0200

    Revert "Update team.ts"
    This reverts commit d59ad648eed7e46c567d293d29e0eed2e8708083.

--- src/operations/team.ts
+++ src/operations/team.ts
@@ -6,7 +6,3 @@ export const countTeamsWhere = (teamArguments: TeamWhereInput): Promise<number> 
     where: teamArguments,
   });
 };
-
-export const fetchTeam = (id: string) => {
-  return database.team.findOne({ where: { id } });
-};
