0a1504f02f814566c05e9ab780bf923a00b8202a
Thomas de Lachaux <thomas.delachaux@gmail.com>
2020-10-11 23:28:29 +0200

    Add sentry (#17)
    * Add sentry
    
    * Correct spelling error
    
    Co-authored-by: Florian Esslinger <32069282+florianess@users.noreply.github.com>
    
    * Add codecov Slack notifications
    
    * Remove slack bot and disable github codevoc checks
    
    Co-authored-by: Florian Esslinger <32069282+florianess@users.noreply.github.com>

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import { apiPort, nodeEnv as nodeEnvironment } from './utils/environment';
 import log from './utils/log';
 import app from './app';
 
-// Listen the API on port 3000 (default)
-app.listen(apiPort(), () => {
-  log.debug(`Node environment: ${nodeEnvironment()}`);
-  log.debug(`Listening on ${apiPort()}...`);
-});
+try {
+  // Listen the API on port 3000 (default)
+  app.listen(apiPort(), () => {
+    log.debug(`Node environment: ${nodeEnvironment()}`);
+    log.debug(`Listening on ${apiPort()}...`);
+  });
+} catch (error) {
+  log.error(error);
+}
