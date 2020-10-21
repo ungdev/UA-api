54778d21866ea13c88e61c0620c8fe7d46c82add
Lucien Charleux <saperli000@gmail.com>
2020-10-22 01:46:04 +0200

    .ts-initUserRequest():
    Fix initUserRequest()
    Fix fetchUser()

--- src/operations/user.ts
+++ src/operations/user.ts
@@ -5,6 +5,10 @@ export const fetchUsers = (): Promise<User[]> => {
   return database.user.findMany();
 };
 
-export const fetchUser = (id: string) => {
-  return database.user.findOne({ where: { id } });
+export const fetchUser = (paramid: string) => {
+  return database.user.findOne({
+    where: {
+      id: paramid,
+    },
+  });
 };
