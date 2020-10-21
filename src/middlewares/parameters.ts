94803aa2ce6f89b202358414e02b4376795a910a
Lucien Charleux <saperli000@gmail.com>
2020-10-22 01:49:14 +0200

    Replace all UserRequest by Request

--- src/middlewares/parameters.ts
+++ src/middlewares/parameters.ts
@@ -1,14 +1,9 @@
-import { Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { unauthenticated, unauthorized } from '../utils/responses';
-import { UserRequest } from '../types';
 import { fetchTeam } from '../operations/team';
 
 // Checks if the user is the captain of the team specified in the URL. If not, it will return an error
-export const isCaptainOfTeamId = async (
-  request: UserRequest,
-  response: Response,
-  next: NextFunction,
-): Promise<void> => {
+export const isCaptainOfTeamId = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
   const { user } = request;
   if (user) {
     const team = await fetchTeam(request.params.teamId);
@@ -21,7 +16,7 @@ export const isCaptainOfTeamId = async (
 };
 
 // Checks if the user is who he pretends to be. If not, it will return an error
-export const isUserId = (request: UserRequest, response: Response, next: NextFunction): void => {
+export const isUserId = (request: Request, response: Response, next: NextFunction): void => {
   const { user } = request;
   if (user) {
     if (user.id === request.params.userId) {
@@ -33,7 +28,7 @@ export const isUserId = (request: UserRequest, response: Response, next: NextFun
 };
 
 // Checks the user is the captain of the team. If not, it will return an error
-export const isInTeamId = (request: UserRequest, response: Response, next: NextFunction): void => {
+export const isInTeamId = (request: Request, response: Response, next: NextFunction): void => {
   const { user } = request;
   if (user) {
     // Compare user's teamId and teamId of the request
