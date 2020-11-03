6f3290564339d17c9bbfc5dcecb281079df49097
florianess <florian.esslinger@live.fr>
2020-11-03 20:47:50 +0100

    fix: ranking score

--- src/scripts/rankingLol.ts
+++ src/scripts/rankingLol.ts
@@ -18,6 +18,7 @@ const csvWriter = createObjectCsvWriter({
     { id: 'ranks', title: 'Ranks' },
     { id: 'scorePlayers', title: 'ScorePlayers' },
     { id: 'teamScore', title: 'TeamScore' },
+    { id: 'complete', title: 'Complete' },
     { id: 'opGG', title: 'opGG' },
   ],
 });
@@ -33,6 +34,7 @@ const scorePalier: Record<string, number[]> = {
   Master: [100, 0],
   Grandmaster: [150, 0],
   Challenger: [300, 0],
+  Level: [0, 0],
 };
 
 const scorePlayer = (rank: string) => {
@@ -40,7 +42,7 @@ const scorePlayer = (rank: string) => {
   if (scorePalier[splitRank[0]][1] === 0) {
     return scorePalier[splitRank[0]][0];
   }
-  return scorePalier[splitRank[0]][0] + scorePalier[splitRank[0]][1] * (Number.parseInt(splitRank[1], 10) - 1);
+  return scorePalier[splitRank[0]][0] + scorePalier[splitRank[0]][1] * (4 - Number.parseInt(splitRank[1], 10));
 };
 
 const rankTeam = async (team: SummonersParticipants) => {
@@ -66,6 +68,7 @@ const rankTeam = async (team: SummonersParticipants) => {
     teamScore: scorePlayers.reduce((a, b) => {
       return a + b;
     }),
+    complete: validNames.length === team.summonersName.length,
   };
 };
 
@@ -75,10 +78,11 @@ const rankTeam = async (team: SummonersParticipants) => {
 
   const tournament = await fetchTournament('lol');
   const toornamentParticipants = await fetchParticipantsSummonersName(tournament.toornamentId);
-  const invalidParticipants = toornamentParticipants.filter((team) => team.summonersName.some((name) => name === ''));
+  let invalidParticipants = toornamentParticipants.filter((team) => team.summonersName.some((name) => name === ''));
+  invalidParticipants = invalidParticipants.map((team) => ({ ...team, complete: false }));
   let validParticipants = toornamentParticipants.filter((team) => team.summonersName.every((name) => name !== ''));
   validParticipants = await Promise.all(validParticipants.map((team) => rankTeam(team)));
-  await csvWriter.writeRecords(validParticipants);
+  await csvWriter.writeRecords([...validParticipants, ...invalidParticipants]);
   process.exit(0);
 })().catch((error) => {
   logger.error(error);
