83bf410c4dc58f44ab175e7f4eff2b4b108fb013
Thomas de Lachaux <thomas.delachaux@gmail.com>
2021-02-24 19:27:06 +0200

    Add GET /admin/users/:userId/carts

--- src/controllers/admin/users/index.ts
+++ src/controllers/admin/users/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import forcePay from './forcePay';
+import getCarts from './getCarts';
 
 const router = Router();
 
+router.get('/:userId/carts', getCarts);
 router.post('/:userId/force-pay', forcePay);
 
 export default router;
