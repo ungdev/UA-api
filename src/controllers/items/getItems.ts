e3c42e78898f934c2bf6a19943e05e297fa01c2e
Saperlu <saperli000@gmail.com>
2021-04-10 17:19:13 +0200

    La billetterie doit être ouverte pour la route /items

--- src/controllers/items/getItems.ts
+++ src/controllers/items/getItems.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
+import { isShopAllowed } from '../../middlewares/settings';
 import { fetchItems } from '../../operations/item';
 import { filterItem } from '../../utils/filters';
 import { success } from '../../utils/responses';
 
 export default [
   // Middlewares
-
+  isShopAllowed,
+  
   // Controller
   async (request: Request, response: Response, next: NextFunction) => {
     try {
