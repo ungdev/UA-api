7a57f21afb770b2c0072acf7179edf3021b53c02
Corentin Chartier <corentinchartier334@gmail.com>
2021-10-29 12:10:21 +0200

    fix(logs): added a test for "500" error

--- tests/admin/logs/logs.test.ts
+++ tests/admin/logs/logs.test.ts
@@ -1,10 +1,11 @@
-import { expect } from 'chai';
 import request from 'supertest';
 import app from '../../../src/app';
 import { createFakeUser } from '../../utils';
 import database from '../../../src/services/database';
-import { Permission, User } from '../../../src/types';
+import { Error, Permission, User } from '../../../src/types';
 import * as userUtils from '../../../src/utils/users';
+import { sandbox } from '../../setup';
+import * as logOperations from '../../../src/operations/log';
 
 describe('GET /admin/logs', () => {
   let admin: User;
@@ -20,16 +21,16 @@ describe('GET /admin/logs', () => {
     await database.user.deleteMany();
   });
 
-  it('should not return logs from invalid request', () =>
-    request(app).get('/admin/logs').set('Authorization', `Bearer ${adminToken}`).expect(400));
+  it('should throw an internal server error', async () => {
+    // Fake the main function to throw
+    sandbox.stub(logOperations, 'fetchLogs').throws('Unexpected error');
 
-  it('should return the logs', async () => {
-    const { body: logs } = await request(app)
+    await request(app)
       .get(`/admin/logs?page=0`)
       .set('Authorization', `Bearer ${adminToken}`)
-      .expect(200);
-    expect(logs.logs).to.be.lengthOf(0);
-    expect(logs.pageIndex).to.be.equal(0);
-    expect(logs.maxPages).to.be.equal(0);
+      .expect(500, { error: Error.InternalServerError });
   });
+
+  it('should not return logs from invalid request', () =>
+    request(app).get('/admin/logs').set('Authorization', `Bearer ${adminToken}`).expect(400));
 });
