3c0d260bc85affbcda678859be39bd974a2b5818
Pulpss <puulps@gmail.com>
2022-10-23 10:36:22 +0200

    feat: added a general AdminItem route
    - Changed the names of the files to better match the project structure
    - Added a GET admin/items route to see all the items at once for an
      admin
    - Added corresponding tests for the new route
    - Added corresponding documentation for the new route

--- src/controllers/admin/items/index.ts
+++ src/controllers/admin/items/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import item from './item';
+import getItem from './getItem';
+import getItems from './getItems';
 import stock from './stock';
 
 const router = Router();
 
 router.patch('/:itemId', stock);
-router.get('/:itemId', item);
+router.get('/:itemId', getItem);
+router.get('//', getItems);
 
 export default router;
