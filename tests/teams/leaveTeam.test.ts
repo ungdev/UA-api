e95da7b7b8f70bf4a6be60cf29cfb3e79e7a7447
Teddy Roncin <teddy.roncin35@gmail.com>
2023-06-30 01:43:05 +0200

    test: added some tests
    * added tests to verify a team in the queue would be moved to the locked teams
    if necessary with routes :
    	- DELETE /teams/current/users/:userId
    	- DELETE /teams/current/users/current
    	- POST /admin/carts/:cartId/refund

--- tests/teams/leaveTeam.test.ts
+++ tests/teams/leaveTeam.test.ts
@@ -10,7 +10,6 @@ import { createFakeTeam, createFakeUser } from '../utils';
 import { generateToken } from '../../src/utils/users';
 import { fetchUser } from '../../src/operations/user';
 import { getCaptain } from '../../src/utils/teams';
-import { fetchTeam } from '../../src/operations/team';
 
 describe('DELETE /teams/current/users/current', () => {
   let user: User;
@@ -20,10 +19,11 @@ describe('DELETE /teams/current/users/current', () => {
   let coachToken: string;
 
   let team: Team;
+  let waitingTeam: Team;
 
   before(async () => {
     const tournament = await tournamentOperations.fetchTournament('rl');
-    team = await createFakeTeam({ members: tournament.playersPerTeam, locked: true, paid: true });
+    team = await createFakeTeam({ members: tournament.playersPerTeam, locked: true, paid: true, tournament: 'rl' });
 
     // Find a user that is not a captain
     user = team.players.find((player) => player.id !== team.captainId);
@@ -32,7 +32,23 @@ describe('DELETE /teams/current/users/current', () => {
     // Create a coach
     coach = await createFakeUser({ type: UserType.coach });
     coachToken = generateToken(coach);
-    teamOperations.joinTeam(team.id, coach, UserType.coach);
+    await teamOperations.joinTeam(team.id, coach, UserType.coach);
+
+    // Fill the tournament
+    const promises = [];
+    for (let index = 0; index < tournament.placesLeft - 1; index++) {
+      promises.push(createFakeTeam({ members: tournament.playersPerTeam, locked: true, paid: true, tournament: 'rl' }));
+    }
+    await Promise.all(promises);
+
+    // Create a team that is in the waiting list
+    waitingTeam = await createFakeTeam({
+      members: tournament.playersPerTeam,
+      locked: false,
+      paid: true,
+      tournament: 'rl',
+    });
+    await teamOperations.lockTeam(waitingTeam.id);
   });
 
   after(async () => {
@@ -66,20 +82,30 @@ describe('DELETE /teams/current/users/current', () => {
 
   it('should successfully quit the team, but not unlock the team as the user is a coach', async () => {
     // Verify the team is locked
-    let databaseTeam = await fetchTeam(team.id);
+    let databaseTeam = await teamOperations.fetchTeam(team.id);
     expect(databaseTeam.lockedAt).to.be.not.null;
     expect(databaseTeam.enteredQueueAt).to.be.null;
 
     // Make the request
     await request(app).delete('/teams/current/users/current').set('Authorization', `Bearer ${coachToken}`).expect(204);
 
+    // Verify the user has been removed from the team
+    const removedUser = await fetchUser(coach.id);
+    expect(removedUser.teamId).to.be.null;
+    expect(removedUser.type).to.be.null;
+
     // Verify the team is still locked
-    databaseTeam = await fetchTeam(team.id);
+    databaseTeam = await teamOperations.fetchTeam(team.id);
     expect(databaseTeam.lockedAt).to.be.not.null;
     expect(databaseTeam.enteredQueueAt).to.be.null;
+
+    // Verify the waiting team is still in the queue
+    waitingTeam = await teamOperations.fetchTeam(waitingTeam.id);
+    expect(waitingTeam.lockedAt).to.be.null;
+    expect(waitingTeam.enteredQueueAt).to.be.not.null;
   });
 
-  it('should successfully quit the team and unlock the team', async () => {
+  it('should successfully quit the team and unlock the team, and lock the waiting team', async () => {
     await request(app).delete('/teams/current/users/current').set('Authorization', `Bearer ${token}`).expect(204);
 
     const removedUser = await fetchUser(user.id);
@@ -88,9 +114,14 @@ describe('DELETE /teams/current/users/current', () => {
     expect(removedUser.type).to.be.null;
 
     // Verify the team has been unlocked
-    const databaseTeam = await fetchTeam(team.id);
+    const databaseTeam = await teamOperations.fetchTeam(team.id);
     expect(databaseTeam.lockedAt).to.be.null;
     expect(databaseTeam.enteredQueueAt).to.be.null;
+
+    // Verify the waiting team has been locked
+    waitingTeam = await teamOperations.fetchTeam(waitingTeam.id);
+    expect(waitingTeam.lockedAt).to.be.not.null;
+    expect(waitingTeam.enteredQueueAt).to.be.null;
 
     // Rejoin the team for next tests
     await teamOperations.joinTeam(team.id, removedUser, UserType.player);
