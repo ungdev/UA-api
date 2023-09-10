98846318f541b2c0ca3c5a6cce10a65ae2e53b08
Noé Landré <noe.landre@laposte.net>
2023-09-10 18:35:33 +0200

    fix: upload compression

--- src/operations/upload.ts
+++ src/operations/upload.ts
@@ -1,10 +1,21 @@
 import axios from 'axios';
 import env from '../utils/env';
+import sharp from 'sharp';
 
 // Operations for file upload
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const uploadFile = async (file: any, path: string, name: string) => {
-  const fileBlob = new Blob([file.buffer], { type: file.mimetype });
+
+  let compressedFile = file.buffer;
+
+  // compress file before upload
+  if(file.mimetype === 'image/jpeg')
+    compressedFile = await sharp(file.buffer).jpeg({quality: 80}).toBuffer();
+  else if(file.mimetype === 'image/png')
+    compressedFile = await sharp(file.buffer).resize(800).png({quality: 100}).toBuffer();
+
+  // create blob
+  const fileBlob = new Blob([compressedFile], { type: file.mimetype });
 
   const formData = new FormData();
   formData.append('file', fileBlob, file.originalname);
