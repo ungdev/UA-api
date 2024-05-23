afe0a440aa91c1c8510e018b6c7a0cccb4bb38c5
Noé Landré <noe.landre@laposte.net>
2024-05-23 11:22:07 +0200

    fix: improve badge coverage

---
+++ tests/admin/badges/generateBadges.test.ts
@@ -0,0 +1,128 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import app from '../../../src/app';
+import database from '../../../src/services/database';
+import { Error, Permission, User, UserType } from '../../../src/types';
+import { createFakeUser } from '../../utils';
+import * as userUtils from '../../../src/utils/users';
+import { updateAdminUser } from '../../../src/operations/user';
+
+describe('POST /badges/generate', () => {
+  let adminUser: User;
+  let adminToken: string;
+  let user: User;
+  let userToken: string;
+
+  before(async () => {
+    adminUser = await createFakeUser({ permissions: [Permission.admin], type: UserType.player });
+    adminToken = userUtils.generateToken(adminUser);
+
+    user = await createFakeUser({ permissions: [Permission.orga] });
+
+    // Add a mainCommission to the user
+    updateAdminUser(user, { orgaMainCommission: 'vieux', orgaRoles: [{ commission: 'vieux', commissionRole: 'member' }] });
+  });
+
+  after(async () => {
+    await database.orgaRole.deleteMany();
+    await database.orga.deleteMany();
+    await database.user.deleteMany();
+  });
+
+  it('should return an error for unauthenticated user', async () => {
+    await request(app).post('/admin/badges').send({}).expect(401, { error: Error.Unauthenticated });
+  });
+
+  it('should return an error for invalid body', async () => {
+    await request(app)
+      .post('/admin/badges')
+      .set('Authorization', `Bearer ${adminToken}`)
+      .send({})
+      .expect(400, { error: Error.InvalidBody });
+  });
+
+  it('should generate badges for orgas', async () => {
+    await request(app)
+      .post('/admin/badges')
+      .set('Authorization', `Bearer ${adminToken}`)
+      .send({
+        fields: [{ type: 'orgas' }],
+      })
+      .expect(200)
+      .then((response) => {
+        expect(response.headers['content-type']).to.equal('application/pdf');
+      });
+  });
+
+  it('should generate custom badges', async () => {
+    await request(app)
+      .post('/admin/badges')
+      .set('Authorization', `Bearer ${adminToken}`)
+      .send({
+        fields: [{ type: 'custom', quantity: 5, permission: 'restricted', name: 'Custom Badge' }],
+      })
+      .expect(200)
+      .then((response) => {
+        expect(response.headers['content-type']).to.equal('application/pdf');
+      });
+  });
+
+  it('should generate a single badge for a specific user', async () => {
+    const orgaUser = await createFakeUser({ permissions: [Permission.orga]});
+
+    // Add a mainCommission to the user
+    updateAdminUser(orgaUser, { orgaMainCommission: 'vieux', orgaRoles: [{ commission: 'vieux', commissionRole: 'member' }] })
+
+    await request(app)
+      .post('/admin/badges')
+      .set('Authorization', `Bearer ${adminToken}`) 
+      .send({
+        fields: [{ type: 'single', email: orgaUser.email }],
+      })
+      .expect(200)
+      .then((response) => {
+        expect(response.headers['content-type']).to.equal('application/pdf');
+      });
+  });
+
+  it('should return an error for a non-existing user in single type', async () => {
+    await request(app)
+      .post('/admin/badges')
+      .set('Authorization', `Bearer ${adminToken}`)
+      .send({
+        fields: [{ type: 'single', email: 'nonexistinguser@example.com' }],
+      })
+      .expect(404, { error: `User (nonexistinguser@example.com) not found` });
+  });
+
+  it('should generate a single custom badge', async () => {
+    await request(app)
+      .post('/admin/badges')
+      .set('Authorization', `Bearer ${adminToken}`)
+      .send({
+        fields: [
+          {
+            type: 'singlecustom',
+            commissionRole: 'member',
+            commissionId: 'vieux',
+            firstname: 'John',
+            lastname: 'Doe',
+          },
+        ],
+      })
+      .expect(200)
+      .then((response) => {
+        expect(response.headers['content-type']).to.equal('application/pdf');
+      });
+  });
+
+  it('should return an error for invalid type', async () => {
+    await request(app)
+      .post('/admin/badges')
+      .set('Authorization', `Bearer ${adminToken}`)
+      .send({
+        fields: [{ type: 'invalidtype' }],
+      })
+      .expect(400, { error: Error.InvalidBody });
+  });
+});
