1860315b20354610b4446a32c7526566a696138c
Remi-pcn <166511249+Remi-pcn@users.noreply.github.com>
2024-10-07 15:03:04 +0200

    Fix api route accessible (#243)
    * feat: create majority filter (#238)
    
    Co-authored-by: Antoine D <antoine.dufils@yahoo.fr>
    Co-authored-by: Remi-pcn <166511249+Remi-pcn@users.noreply.github.com>
    
    * fix: plus d'info sensible quand on cherche /api/teams/:teamID
    
    * fix: changed filter
    
    * fix: badge in descending order
    
    ---------
    
    Co-authored-by: Antoine D <106921102+Suboyyy@users.noreply.github.com>
    Co-authored-by: Antoine D <antoine.dufils@yahoo.fr>

--- src/controllers/admin/badges/generateBadges.ts
+++ src/controllers/admin/badges/generateBadges.ts
@@ -69,6 +69,9 @@ export default [
           case 'orgas': {
             await database.user
               .findMany({
+                orderBy: {
+                  firstname: 'desc',
+                },
                 where: {
                   permissions: { contains: 'orga' },
                 },
