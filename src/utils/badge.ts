6bd1386153b57290afa4eda2cf8d47d6f082203e
Antoine Dufils <antoine.dufils@utt.fr>
2024-10-11 16:34:30 +0200

    fix: text color depending of the badge color (daltonian friendly)

--- src/utils/badge.ts
+++ src/utils/badge.ts
@@ -146,16 +146,19 @@ export const generateBadge = async (badges: Badge[]) => {
         }
       }
 
-      // Place the text containing the name is the bottom middle in bold and in uppercase
-      // Define a text format
-      const textFormat = document.font(fontFamily).fill([23, 18, 74]).fontSize(fontSize);
-
       // 'for' because I dont like to repeat but I like potatoes and pain au chocolat
       for (let col = 0; col < columns; col++) {
         for (let row = 0; row < rows; row++) {
           const index = page * columns * rows + col * rows + row;
 
           if (index >= badges.length) break;
+
+          // Place the text containing the name is the bottom middle in bold and in uppercase
+          // Define a text format
+          const textFormat =
+            badges[index].type === 'fullaccess'
+              ? document.font(fontFamily).fill([239, 220, 235]).fontSize(fontSize)
+              : document.font(fontFamily).fill([23, 18, 74]).fontSize(fontSize);
 
           // Informations about badge
           const lastName = `${badges[index].lastName || ' '}`;
