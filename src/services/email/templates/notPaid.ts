a1661e59f99dc707be1c3a58907715d7fa56f3f8
Antoine D <antoine.dufils@yahoo.fr>
2024-11-26 17:45:41 +0100

    fix: comment notPaid template tournament cancel

--- src/services/email/templates/notPaid.ts
+++ src/services/email/templates/notPaid.ts
@@ -16,7 +16,7 @@ export const generateNotPaidEmail = (user: Omit<RawUser, 'permissions'>) =>
         title: "Ton inscription n'a pas été confirmée",
         components: [
           "L'UTT Arena approche à grand pas, et ton inscription n'est pas encore confirmée. Pour verrouiller ta place, il ne te reste plus qu'à la payer en accédant à la boutique sur le site. \nSi le tournoi auquel tu souhaites participer est d'ores-et-déjà rempli, tu sera placé en file d'attente.",
-          "\n_Si le taux de remplissage d'un tournoi est trop faible d'ici à deux semaines de l'évènement, l'équipe organisatrice se réserve le droit de l'annuler._",
+          // "\n_Si le taux de remplissage d'un tournoi est trop faible d'ici à deux semaines de l'évènement, l'équipe organisatrice se réserve le droit de l'annuler._",
           {
             location: 'https://arena.utt.fr/dashboard/team',
             name: 'Accéder à arena.utt.fr',
