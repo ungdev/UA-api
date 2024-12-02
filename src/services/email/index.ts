6d3e03865f37ae90a35ce1acb387da3dd6fc9b1b
Antoine D <106921102+Suboyyy@users.noreply.github.com>
2024-12-02 11:44:35 +0100

    Feat/send ticket (#268)
    * feat: new email system
    
    * feat: add General Mail route
    
    * feat: back to street legal
    
    * feat: add sendTemplate
    
    * fix: it's better with .fr
    
    * fix: update email template topics
    
    * fix: add return and fix preview undefined
    
    * fix: change minor mail target
    
    * fix: lint
    
    * fix: lint2
    
    * test: disable mail tests
    
    * test: disable other mail test
    
    * feat: mail smtp
    
    * Update .env.example
    
    * Update notPaid.ts
    
    * Update env.ts
    
    * fix: serialized mail now handle attachment
    
    * fix: lint
    
    ---------
    
    Co-authored-by: Noé Landré <noe.landre@laposte.net>
    Co-authored-by: Arthur Dodin <dodinarthur@gmail.com>
    Co-authored-by: Antoine D <antoine.dufils@yahoo.fr>

--- src/services/email/index.ts
+++ src/services/email/index.ts
@@ -4,7 +4,7 @@ import { readFile } from 'fs/promises';
 import { render } from 'mustache';
 import nodemailer from 'nodemailer';
 import { Log } from '@prisma/client';
-import { EmailAttachement, RawUser, MailQuery, User } from '../../types';
+import { RawUser, MailQuery, User } from '../../types';
 import env from '../../utils/env';
 import logger from '../../utils/logger';
 import type { Component, Mail, SerializedMail } from './types';
@@ -41,6 +41,7 @@ export const serialize = async (content: Mail) => {
           typeof text === 'string' ? escapeText(text) : text.map(inflate).join(''),
       },
     ),
+    attachments: content.attachments,
   };
 };
 
@@ -100,16 +101,15 @@ export const transporter = nodemailer.createTransport(emailOptions);
  *  ]
  * }))
  */
-export const sendEmail = async (mail: SerializedMail, attachments?: EmailAttachement[]) => {
+export const sendEmail = async (mail: SerializedMail) => {
   const from = `${env.email.sender.name} <${env.email.sender.address}>`;
-
   try {
     await transporter.sendMail({
       from,
       to: mail.to,
       subject: mail.subject,
       html: mail.html,
-      attachments,
+      attachments: mail.attachments,
     });
 
     logger.info(`Email sent to ${mail.to}`);
@@ -138,7 +138,8 @@ export const sendMailsFromTemplate = async (template: string, targets: any[]) =>
     if (targets.length > 1) {
       const outgoingMails = await Promise.allSettled(
         targets.map(async (target) => {
-          await sendEmail(await mailTemplate(target));
+          const mail = await mailTemplate(target);
+          await sendEmail(mail);
         }),
       );
 
@@ -162,8 +163,8 @@ export const sendMailsFromTemplate = async (template: string, targets: any[]) =>
       console.info(`\tMails envoyés: ${results.delivered}\n\tMails non envoyés: ${results.undelivered}`);
       return results;
     }
-
-    return sendEmail(await mailTemplate(targets[0]));
+    const mail = await mailTemplate(targets[0]);
+    return sendEmail(mail);
   } catch (error) {
     logger.error('Error while sending emails', error);
     return false;
